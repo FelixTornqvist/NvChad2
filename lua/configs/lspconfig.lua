de005a4ece81c2cfc33c294f9827e57b0bc6f68b
Felix Törnqvist <felix.trnqvist@gmail.com>
2025-12-13 14:54:57 +0100

    Rust lsp, etc

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls" }-- "typescript-language-server" }
+local servers = { "html", "cssls", "ts_ls", "rust_analyzer" }-- "typescript-language-server" }
 vim.lsp.enable(servers)
 
 -- vim.lsp.enable("tsserver", {
